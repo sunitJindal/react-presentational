d906e864ffa3f463d91f6befed0ad7638240e16b
Sunit Jindal <sunitjindal@gmail.com>
2017-06-02 22:04:31 +0530

    Bug fix and test case for presentationalTest.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var presentationalIndexTemplate = require('./lib/templates/presentationalIndex');
 var presentationalTemplate = require('./lib/templates/presentational');
 var cssTemplate = require('./lib/templates/css');
+var presentationalTestTemplate = require('./lib/templates/presentationalTest');
 
 var componentName = process.argv[2];
 
@@ -20,9 +21,12 @@ var cssTemplate = cssTemplate(componentName);
 
 var componentTemplate = presentationalTemplate(componentName);
 
+var componentTestTemplate = presentationalTestTemplate(componentName);
+
+
 fs.writeFileSync(`${dir}/index.js`, indexTemplate);
 fs.writeFileSync(`${dir}/${componentName}.css`, cssTemplate);
 fs.writeFileSync(`${dir}/${componentName}.js`, componentTemplate);
-
+fs.writeFileSync(`${dir}/${componentName}.spec.js`, componentTestTemplate);
 
 
